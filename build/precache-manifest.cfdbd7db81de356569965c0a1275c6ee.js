6ea7fa13c7b85a87df157a84ef1cf42f68d7fe60
M4Anton <maciek.antonczak@wp.pl>
2020-02-16 16:41:18 +0100

    build added

---
+++ build/precache-manifest.cfdbd7db81de356569965c0a1275c6ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "221a2bcea290676da76acf367315256d",
+    "url": "/ZtM-RoboFriendsApp/index.html"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/ZtM-RoboFriendsApp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "70cf2c12f9b95390e70d",
+    "url": "/ZtM-RoboFriendsApp/static/css/main.c53538ce.chunk.css"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/ZtM-RoboFriendsApp/static/js/2.4b6b2329.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ZtM-RoboFriendsApp/static/js/2.4b6b2329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70cf2c12f9b95390e70d",
+    "url": "/ZtM-RoboFriendsApp/static/js/main.1793607a.chunk.js"
+  },
+  {
+    "revision": "c3456641b0de99a810ea",
+    "url": "/ZtM-RoboFriendsApp/static/js/runtime-main.a6d5e6c1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ZtM-RoboFriendsApp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
